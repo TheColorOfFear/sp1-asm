b77a00b85665f42c87248e9c9a2c9804e87667c9
TheColorOfFear <68704661+TheColorOfFear@users.noreply.github.com>
2023-06-22 03:32:55 -0500

    Dynamic goto length system, extra compilation args.

--- sp1asm.py
+++ sp1asm.py
@@ -1,11 +1,38 @@
 print("\033c")
 print("SP1 ASSEMBLER || (C)2023  723179\n")
+
+def lenHex(funcLength):
+    funcOutput = hex(funcLength)[2:]
+    while len(funcOutput) < 2:
+        funcOutput = "0" + funcOutput
+    return funcOutput
+
+#define some default variables
+##does it print the location of every jump point?
+jpPrint = False
+
 # get input and choose output filename
 with open(input("File input name? "), "r", encoding="utf-8") as f:
     file = f.read()
     fileIn = file.splitlines()
     length = len(str(len(file))) + 1
 outName = input("File output name? ")
+
+while 1 == 1:
+    arg = input("Extra Arguments? ")
+    if arg == "":
+        break
+    if arg[0] in ["l","j"]:
+        #if the automatic get-length thing breaks the program, here's an option to fix it preemptively
+        if arg[0] == "l":
+            length = int(arg[1:])
+        elif arg[0] == "j":
+            if arg[1] == "0":
+                jpPrint = False
+            else:
+                jpPrint = True
+    else:
+        print("Syntax Error")
 
 ## Variables
 currentPos = 0
@@ -18,10 +45,11 @@ for i in range(len(fileIn)):
     if fileIn[i][0] == "j":
         jpName.append(fileIn[i][1:])
         jpPos.append(currentPos + 1)
-        print("JP: " + fileIn[i][1:] + " :" + str(currentPos + 1))
+        if jpPrint:
+            print("JP: " + fileIn[i][1:] + " :" + str(currentPos + 1))
     else:
-        if fileIn[i][0] == "=" or fileIn[i][0] == "g":
-            currentPos += 8
+        if fileIn[i][0] in ["=", "g"]:
+            currentPos += (4 + length)
         else:
             currentPos += len(fileIn[i])
 
@@ -30,16 +58,14 @@ output = ""
 for i in range(len(fileIn)):
     if fileIn[i][0] == "j":
         pass
-    elif fileIn[i][0] == "g":
-        output += "0604"
+    elif fileIn[i][0] in ["=","g"]:
+        if fileIn[i][0] == "g":
+            output += "06"
+        elif fileIn[i][0] == "=":
+            output += "20"
+        output += lenHex(length)
         tempOut = str(jpPos[jpName.index(fileIn[i][1:])])
-        while len(tempOut) < 4:
-            tempOut = "0" + tempOut
-        output += tempOut
-    elif fileIn[i][0] == "=":
-        output += "2004"
-        tempOut = str(jpPos[jpName.index(fileIn[i][1:])])
-        while len(tempOut) < 4:
+        while len(tempOut) < length:
             tempOut = "0" + tempOut
         output += tempOut
     else:
